f762da9240b26dc7e4d26a53b41728ef2d43f079
pravanjaDev <pravanjan.niranjan@a-cti.com>
2018-02-14 21:06:02 +0530

    commiting to master

---
+++ src/main/java/v0/com/ninja/datamodel/NinjaDAO.java
@@ -0,0 +1,56 @@
+package v0.com.ninja.datamodel;
+
+
+
+import java.util.List;
+
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+import v0.com.ninja.jdo.Speaker;
+import v0.com.ninja.util.PersistenceManagerUtil;
+
+
+
+public class NinjaDAO {
+	private static final Logger logger = Logger.getLogger(NinjaDAO.class.getName());
+	
+	
+	
+	public static Speaker getSpeaker(String name ) {
+		PersistenceManager  pm = PersistenceManagerUtil.getPersistanceManager();
+
+		Speaker speaker = null;
+		try {
+			logger.info("step list  *********** ");
+
+					
+			Query query = pm.newQuery(Speaker.class,"name =='"+name+"'");
+			List<Speaker> spList = (List<Speaker>) query.execute();
+
+			spList = (List<Speaker>) pm.detachCopyAll(spList);
+			if(spList != null && spList.size() > 0){
+				speaker =  spList.get(0);
+			}
+			
+		}
+		catch(Exception e){
+		
+			logger.log(Level.ERROR,
+					"\n An error has occured while persisting : " + "\n", e);
+		}
+		
+		return speaker;
+	}
+
+	
+	
+
+	
+
+
+
+}
